7c8a04f39425440b75612394fa0fad2570fb2a2c
Guilherme Zuriel <guizuriel.dev@gmail.com>
2024-08-08 15:15:57 -0300

    Incrementador para repetir tasks

--- src/main/java/org/example/guilhermezuriel/springbatchproject/BatchConfig.java
+++ src/main/java/org/example/guilhermezuriel/springbatchproject/BatchConfig.java
@@ -4,10 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,14 +24,22 @@ public class BatchConfig {
     private final PlatformTransactionManager transactionManager;
 
     @Bean
-    Job imprimeOla(){
-        return new JobBuilder("imprimeOla", jobRepository).start(imprimeOlaStep()).build();
+    public Job imprimeOla(){
+        return new JobBuilder("imprimeOla", jobRepository).start(imprimeOlaStep()).incrementer(new RunIdIncrementer()).build();
     }
 
-    Step imprimeOlaStep(){
-        return new StepBuilder("imprimeOla", jobRepository).tasklet((StepContribution contribution, ChunkContext chunkContext) -> {
-                System.out.println("Ola Mundo");
-                return RepeatStatus.FINISHED;
-            }, transactionManager).build();
+    public Step imprimeOlaStep(){
+        return new StepBuilder("imprimeOla", jobRepository).tasklet(imprimeOlaTasklet("Guilherme"), transactionManager).build();
+    }
+
+
+    public Tasklet imprimeOlaTasklet(String nome){
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                    System.out.printf("Ola %s", nome);
+                    return RepeatStatus.FINISHED;
+            }
+        };
     }
 }
